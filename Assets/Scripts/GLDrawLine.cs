4e349998478aee6c02cdc954909a9c3cc5dfdc1e
Daybson Paisante <daybson.paisante@outlook.com>
2016-02-06 12:47:43 -0200

    First commit

---
+++ Assets/Scripts/GLDrawLine.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GLDrawLine : MonoBehaviour
+{
+    public Material mat;
+    public Transform[] points;
+
+    void OnPostRender()
+    {
+        if (!mat)
+        {
+            Debug.LogError("Please Assign a material on the inspector");
+            return;
+        }
+
+        GL.PushMatrix();
+        mat.SetPass(0);
+        GL.Begin(GL.LINES);
+        for (var i = 1; i < this.points.Length; i++)
+        {
+            GL.Vertex(this.points[i].position);
+            GL.Vertex(this.points[i - 1].position);
+        }
+        GL.End();
+        GL.PopMatrix();
+    }
+}
